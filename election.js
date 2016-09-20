b8424ec759f383fcb8f3df55860c72dfd30427b7
titli1981 <madhumishra084@gmailcom>
2016-09-20 06:24:45 -0400

    created an ajax request for a JSON response and looped through the JSON objects and appended each name/number of vote.

--- election.js
+++ election.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
+$.ajax({
+    url:"https://bb-election-api.herokuapp.com/",
+    method:'GET',
+    //data:{} ,
+    dataType: "JSON"
+}).done(function(responseData){
+    var candidateJson = responseData
+    var candidateList = candidateJson['candidates']
+    var candidateContainer = $("<ul>")
+
+    candidateList.forEach(function(data,index){
+        console.log(data)
+        $('<li>').html('Name:' + data.name + ',Votes:'+ data.votes ).appendTo(candidateContainer)
+        //$(candidateContainer).append('<li>').html('Name:' + data.name + ',Votes:'+ data.votes )
+
+    });
+    $('body').append(candidateContainer)
+});
+
 
   // Imagination!
 
